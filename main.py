de0e83650e984b4e6ea2b4472c12e26137420787
Alphabeit <104557286+Alphabeit@users.noreply.github.com>
2023-11-01 12:58:28 +0100

    v0.1.0

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 #!./venv/bin python3
  # Creator: Christopher / Alphabeit
- # v0.0.1
- # last Update: 20231026
+ # v0.1.0
+ # last Update: 20231029
 
-# from Functions import Report   # standard, report
-from Functions import UserChoice   # Main function, Choice-Menu, starts all function for user
+from Functions import Menu   # Main function, Choice-Menu, starts all function for user
 
 
 
 want_to_exit = False   # Loop condition, as long as False -> the loop will run --- the stop-order, resp. the True come from Module -> UserChoice
 
 while not want_to_exit is True:
-    # reports, give the curently status of money_balance
-    # Reports.sss
     # Choice_Menu, starts all another functions
-    want_to_exit = UserChoice.choice_menu()   # starts function and letzt return the loop condition
+    want_to_exit = Menu.Terminal_menu()  # starts function and letzt return the loop condition
 
 
 
