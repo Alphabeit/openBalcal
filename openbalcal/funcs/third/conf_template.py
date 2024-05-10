4b56c41a9c500e611fd77ee312fb9f699c97a478
Alphabeit <104557286+Alphabeit@users.noreply.github.com>
2024-05-10 15:25:12 +0200

    Update conf_template.py

--- openbalcal/funcs/third/conf_template.py
+++ openbalcal/funcs/third/conf_template.py
@@ -7,12 +7,12 @@ def Template():
     template = ('''version: v0.3.0
 config:
   paths:
-    database: ./0etc/openbalcal/database.csv
-    topics: ./0etc/openbalcal/topics.csv
+    database: /etc/openbalcal/database.csv
+    topics: /etc/openbalcal/topics.csv
     
   country:
     currency: DOLLAR
-    lang: ./0bin/openbalcal/lang/en_US.yml
+    lang: /bin/openbalcal/lang/en_US.yml
   
   funcs:
     report: all:today,all:this_month''')
